44c57964c4b338046b942aeb9d5a4191c132d553
MatheusCorreaaa <mcorreia073@gmail.com>
2024-07-09 03:10:19 -0300

    primeiro commit

---
+++ src/app/cadastro/cadastro.component.ts
@@ -0,0 +1,52 @@
+import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ConsultaCepService } from '../service/consulta-cep.service';
+
+@Component({
+  selector: 'app-cadastro',
+  templateUrl: './cadastro.component.html',
+  styleUrls: ['./cadastro.component.css']
+})
+export class CadastroComponent implements OnInit {
+
+  constructor(private router: Router,
+    private consultacepService: ConsultaCepService) { }
+
+  ngOnInit(): void {
+  }
+
+  consultaCEP(ev:any,f:NgForm){
+    const cep =ev.target.value;
+    if(cep !== ""){
+      this.consultacepService.getConsultaCep(cep).subscribe((resultado) => 
+        {
+        console.log (resultado);
+        this.populandoEndereco(resultado,f);
+      });
+      
+    }
+    
+  }
+
+  populandoEndereco(dados:any,f:NgForm){
+    f.form.patchValue({
+      endereco: dados.logradouro,
+      complemento: dados.complemento,
+      bairro: dados.bairro,
+      cidade: dados.localidade,
+      estado: dados.uf
+    })
+  }
+
+
+
+  cadastrar(form: NgForm){
+    if(form.valid){
+      this.router.navigate(['./sucesso'])
+    }else{
+      alert('Formulário inválido')  
+    }
+      console.log(form.controls);
+  }
+}
